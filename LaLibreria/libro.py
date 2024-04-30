b3c37cc772e86a4c994f8d5704670e6a1707e79f
tatiAndonegui <tatiana.andonegui@davinci.edu.ar>
2024-04-30 20:25:07 -0300

    Clase libro: creación de contructor, getters y planteo de los metodos

---
+++ LaLibreria/libro.py
@@ -0,0 +1,46 @@
+class Libro:
+
+  def __init__(self, titulo,precio, autor, idioma, editorial, anio_publicacion, stock,categoria):
+    self.titulo = titulo 
+    self.precio = precio
+    self.autor = autor
+    self.idioma = idioma
+    self.editorial = editorial
+    self.anio_publicacion = anio_publicacion
+    self.stock = stock
+    self.categoria = categoria
+
+#METODOS
+def getTitulos(self):
+  return self.titulo
+def getPrecio(self):
+  return self.precio
+def getAutor(self):
+  return self.autor
+def getIdioma(self):
+  return self.idioma
+def getEditorial(self):
+  return self.editorial
+def getAnio_publicacion(self):
+  return self.anio_publicacion
+def getStock(self):
+  return self.stock
+def getCategoria(self):
+  return self.categoria
+
+def mostrarLibro(self):
+  print("\nTitulo: " + self.getTitulo()
+        + "\nPrecio: " + self.getPrecio() 
+        + "\nAutor: " + self.getAutor() 
+        + "\nIdioma: " + self.getIdioma() 
+        + "\nEdtorial:" + self.getEditorial() 
+        + "\nAnio de publicacion: " + self.getAnio_publicacion() 
+        + "\nStock: " + self.getStock() 
+        + "\nCategoria: " + self.getCategoria())
+
+def modificarLibro(self):
+  #aca crear el metodo para modificar los datos del libro
+
+def ajustarPrecio(self, precio):
+  self.precio = precio
+   #aca crear el metodo para modificar el precio
